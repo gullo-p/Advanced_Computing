ac0498386e5b2e9b6d800a8cf7a426a8d5d1e9aa
Guglielmo <guglielmo.pelino@barcelonagse.eu>
2016-01-12 11:15:10 +0100

    Latest update

--- PS1/genData.R
+++ PS1/genData.R
@@ -1,4 +1,6 @@
 if (!require("extrafont")) install.packages("extrafont"); library(extrafont) 
+library(ggplot2)
+
 
 ###producing the covariance matrix
 sigmaXY <- function(rho, sdX, sdY) {
@@ -16,7 +18,7 @@ genBVN <- function(n = 1, seed = NA, muXY=c(0,1), sigmaXY=diag(2)) {
   return(rdraws)
 }
 
-##function for producing the plot and saving in pdf
+##function for producing the plot and saving it in pdf
 plot.pdf <- function(Df){
   pdf("dataPlot.pdf", width=4, height=4.5)
   a <- ggplot(data = Df, 
@@ -32,10 +34,12 @@ plot.pdf <- function(Df){
 
 
 
-library(ggplot2)
+
 #font_import(), uncomment if required
 
 #main function
+#the double indexes 00,01,10,11 model two subcategories for each of the two categories.
+#Final outputs of the function are four different clusters of points.
 Data <- function(no00, no01, no10, no11, mu00, mu01, mu10, mu11, sd00, 
                      sd01, sd10, sd11, rho00, rho01, rho10, rho11, seed=1111, save = TRUE) {
   sigma00 <- sigmaXY(rho=rho00, sdX=sd00[1], sdY=sd00[2])
